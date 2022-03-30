4d3be47b721134a2368196861903f49b5ac28b62
BennyCao <caobenny122@gmail.com>
2022-03-30 14:34:41 -0700

    fixed bubbling issues

--- src/app.js
+++ src/app.js
@@ -1,15 +1,15 @@
 /* selects the specified class/element*/
-const selectDiv = document.querySelector(".original-container")
+const selectDiv = document.querySelectorAll(".original-container div")
 const cloneDiv = document.querySelector(".copy-container")
 const clearButton = document.querySelector("button")
 
-selectDiv.addEventListener('click', (evt) => {
+selectDiv.forEach(iterate => iterate.addEventListener('click', (evt) => {
     /* the line below selects*/
     const whateverbeingcopied = evt.target.parentElement.outerHTML
     /* clones whatever is clicked */
     cloneDiv.insertAdjacentHTML("beforeend", whateverbeingcopied)
     /* stops you from cloning the whole div */
-})
+}))
 
 
 /* click button to clear stuff from copy container */
